4abf80aa9f590b95b343345003a9ee59f4cf763f
haindleontec <haind@leontec.co.jp>
2024-10-09 16:03:29 +0700

    dinh vl

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import sequelize from './models/index';
-// import User from './models/user';
 import userRouter from './routes/userRoutes';
 
 const express = require('express');
@@ -15,21 +14,6 @@ server.use(express.json());  // For parsing application/json
  * Define router
  */
 server.use('/users', userRouter);
-
-// server.post('/users', async (req, res) => {
-//     try {
-//         const { name, email } = req.body;
-//         const user = await User.create({ name, email });
-//         res.json(user);
-//     } catch (error) {
-//         res.status(500).json({ error: 'Failed to create user' });
-//     }
-// });
-
-// server.get('/users', async (req, res) => {
-//     const users = await User.findAll();
-//     res.json(users);
-// });
 
 sequelize.sync({ force: false }).then(() => {
 });
